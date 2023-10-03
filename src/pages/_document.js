cc664c3927b1f0129b9a673f70a57d29abd29d1f
Bilesanmi farouk <bilesanmifaruk@gmail.com>
2023-10-03 22:40:03 +0100

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,11 +16,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="React Nextjs Template Archo onepage themeforest"
+            content="SGArchi"
           />
           <meta
             name="description"
-            content="Archo - Onepage Multi-Purpose React nextjs Template"
+            content="SGArchi"
           />
           <meta name="author" content="" />
 
